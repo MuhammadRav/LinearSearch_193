239ab199fe0eff44e4c1c7e9d46bad5b756b8428
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-04-14 10:33:52 +0700

    ss

--- Linear_Search/Linear_Search.cpp
+++ Linear_Search/Linear_Search.cpp
@@ -5,4 +5,17 @@ int arr[20];		// Array to be search
 int n;				// Number of selements in the Array
 int i;				// Index of Array element
 
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of elements in the Array: ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+	}
+}
+
 int main()
